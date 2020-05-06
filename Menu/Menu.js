fe42e6bd83a198f511fd8b7adfeacac64dfd5bf4
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-06 19:35:06 -0400

    Flex assignment

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,17 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+function createMenu(menu){
+  const menuHide = document.querySelector('.menu-button')
+  const menuDiv = document.createElement('div')
+  const unorderedList = document.createElement('ul')
+  
+  menuDiv.appendChild(unorderedList)
+  menu.forEach(function(item){
+
+    const listItem = document.createElement('li')
+    listItem.textContent = item;
+    unorderedList.appendChild(listItem)
+  })
+  
+}
